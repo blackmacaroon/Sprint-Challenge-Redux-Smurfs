f96527f874a13b5d6e9c880f596be518e66812ca
Kayla <6lackmacaroon@gmail.com>
2019-05-17 09:50:41 -0600

    built first action creator getSmurfs

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,7 +1,16 @@
+import axios from 'axios';
 /* 
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+export const FETCHING = 'FETCH';
+export const FETCH_WIN = 'FETCH_WIN';
+export const FETCH_FAIL = 'FETCH_FAIL';
+export const ADDING = 'ADDING';
+export const ADD_WIN = 'ADD_WIN';
+export const ADD_FAIL = 'ADD_FAIL';
+export const UPDATING = 'UPDATING';
+export const DELETING = 'DELETING';
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
@@ -13,3 +22,16 @@
    U - updateSmurf
    D - deleteSmurf
 */
+export const getSmurfs = () => dispatch => {
+  dispatch({ type: FETCHING });
+  return axios
+    .get('http://localhost:3333/smurfs')
+    .then(res => {
+      console.log('fetched!', res)
+      dispatch({ FETCH_WIN, payload: res.data })
+    })
+    .catch (err => {
+      console.log('fetch fail', err)
+      dispatch({ FETCH_FAIL, err})
+    })
+}
